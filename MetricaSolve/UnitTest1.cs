49e220cc57871723503544ccfbb3d48f51f4fd32
Asif Hasan <72686397+AsifHasanChowdhury@users.noreply.github.com>
2023-01-19 16:08:48 +0600

    Major Update

--- MetricaSolve/UnitTest1.cs
+++ MetricaSolve/UnitTest1.cs
@@ -1,5 +1,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V106.IndexedDB;
+using OpenQA.Selenium.Support.UI;
 
 namespace MetricaSolve
 {
@@ -35,22 +37,109 @@ namespace MetricaSolve
             Thread.Sleep(3000);
             var drpdwn = driver.FindElement(By
                 .XPath("//*[@id=\"handsontable\"]/div[1]/div[1]/div/div[1]/table/tbody/tr[1]/td[1]"));
+
+            Thread.Sleep(3000);
+
             drpdwn.Click();
             drpdwn.Click();
-            Thread.Sleep(4000);
-            drpdwn.SendKeys("SECL");
-            //driver.FindElement(By.ClassName("htAutocompleteArrow")).Click();
-            //driver.FindElement(By.ClassName("htAutocompleteArrow")).Click();
+
+            Thread.Sleep(2000);
+            /*
+            var drpdwnvalue = driver
+                .FindElement(By
+                .XPath("/html/body/div[1]/aside[2]/section[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/div/div[1]/div[1]/div/div[1]/table/tbody/tr[1]/td"));
+            
+            drpdwnvalue.Click();
+            */
 
 
-            //drpdwn.SendKeys("SECL");
+            var projectContainer = driver.FindElement(By
+            .XPath("/html/body/div[1]/aside[2]/section[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/textarea"));           
 
-            //driver.FindElement(By
-            //  .XPath("//*[@id=\"handsontable\"]/div[1]/div[1]/div/div[1]/table/tbody/tr[1]/td[1]")).SendKeys("SECL");
+            projectContainer.SendKeys("SECL");
+            projectContainer.SendKeys(Keys.Enter);
 
 
 
+            var TaskTypedrpdwn = driver.FindElement(By
+               .XPath("/html/body/div[1]/aside[2]/section[2]/div[2]/div/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/table/tbody/tr[1]/td[2]"));
 
+            Thread.Sleep(3000);
+
+            TaskTypedrpdwn.Click();
+            TaskTypedrpdwn.Click();
+
+
+            var taskContainer = driver
+                .FindElement(By.XPath("/html/body/div[1]/aside[2]/section[2]/div[2]/div/div/div[2]/div/div[1]/div[3]/textarea"));
+
+            taskContainer.SendKeys("Training");
+            taskContainer.SendKeys(Keys.Enter);
+            // container.SendKeys(Keys.ArrowDown);
+
+
+            var TaskDescripDrpDwn = driver
+                .FindElement(By
+                .XPath("/html/body/div[1]/aside[2]/section[2]/div[2]/div/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/table/tbody/tr[1]/td[3]"));
+
+            TaskDescripDrpDwn.Click();
+            TaskDescripDrpDwn.Click();
+
+            var taskDescriptionContainer = driver
+                .FindElement(By
+                .XPath("/html/body/div[1]/aside[2]/section[2]/div[2]/div/div/div[2]/div/div[1]/div[4]/textarea"));
+
+            taskDescriptionContainer.SendKeys("Test");
+            taskDescriptionContainer.SendKeys(Keys.Enter);
+
+
+            var FromDrpdwn = driver
+            .FindElement(By
+            .XPath("/html/body/div[1]/aside[2]/section[2]/div[2]/div/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/table/tbody/tr[1]/td[4]"));
+            
+            FromDrpdwn.Click();
+            FromDrpdwn.Click();
+
+
+            var FromTimeContainer = driver
+                .FindElement(By
+                .XPath("/html/body/div[1]/aside[2]/section[2]/div[2]/div/div/div[2]/div/div[1]/div[4]/textarea"));
+
+            FromTimeContainer.SendKeys("9:00");
+
+
+            var ToDrpDwn = driver
+                .FindElement(By
+                .XPath("/html/body/div[1]/aside[2]/section[2]/div[2]/div/div/div[2]/div/div[1]/div[1]/div[1]/div/div[1]/table/tbody/tr[1]/td[5]"));
+
+            ToDrpDwn.Click();
+            ToDrpDwn.Click();
+            ToDrpDwn.Click();
+            ToDrpDwn.Click();
+            ToDrpDwn.Click();
+            ToDrpDwn.Click();
+            ToDrpDwn.Click();
+            ToDrpDwn.Click();
+            ToDrpDwn.Click();
+            ToDrpDwn.Click();
+            ToDrpDwn.Click();
+            ToDrpDwn.Click();
+            ToDrpDwn.SendKeys(Keys.Enter);
+
+
+            var ToTimeContainer = driver
+                .FindElement(By
+                .XPath("/html/body/div[1]/aside[2]/section[2]/div[2]/div/div/div[2]/div/div[1]/div[4]/textarea"));
+
+
+            ToTimeContainer.Click();
+            ToTimeContainer.Click();
+
+
+            //IJavaScriptExecutor jse = (JavascriptExecutor)driver;
+            //jse.executeScript("document.getElementById('elementID').setAttribute('value', 'new value for element')");
+            //IJavaScriptExecutor j = (IJavaScriptExecutor)driver;
+            //j.ExecuteScript("document.getElementsByClassName('htAutocomplete').innerHTML ='SECL'");
 
 
             //driver.Close();
